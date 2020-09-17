79375bd376cb90f3d910d09a872a2fde95b96d61
Jacob G. Acosta <jacob.gacosta@gmail.com>
2020-09-17 14:54:11 -0500

    Remove unused code

--- providers/src/main/kotlin/com/koupper/providers/db/DBServiceProvider.kt
+++ providers/src/main/kotlin/com/koupper/providers/db/DBServiceProvider.kt
@@ -2,17 +2,9 @@ package com.koupper.providers.db
 
 import com.koupper.container.app
 import com.koupper.providers.ServiceProvider
-import com.koupper.providers.parsing.TextParser
 
 class DBServiceProvider : ServiceProvider() {
-    private lateinit var currentDirectory: String
-    private lateinit var parser: TextParser
-
     override fun up() {
-        this.currentDirectory = System.getProperty("user.dir")
-
-        this.parser = app.createInstanceOf(TextParser::class, "TextParserEnvPropertiesTemplate")
-
         this.registerPostgres()
 
         this.registerSQLite()
