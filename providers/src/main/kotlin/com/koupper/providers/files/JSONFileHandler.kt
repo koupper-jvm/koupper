18fd3fd12a10e2c260d9761b97d78628a61e681d
Jacob G. Acosta <jacob.gacosta@gmail.com>
2024-10-31 11:40:34 -0600

    Add missing methods to JSOON file handlers

--- providers/src/main/kotlin/com/koupper/providers/files/JSONFileHandler.kt
+++ providers/src/main/kotlin/com/koupper/providers/files/JSONFileHandler.kt
@@ -1,9 +1,11 @@
 package com.koupper.providers.files
 
-interface JSONFileHandler <String, out T> {
-    fun readFrom(filePath: String): JSONFileHandler<String, T>
-
-    fun read(jsonTxt: String): JSONFileHandler<String, T>
-
-    fun getText() : String
+interface JSONFileHandler<T> {
+    fun readFrom(filePath: String): JSONFileHandler<T>
+    fun read(jsonTxt: String): JSONFileHandler<T>
+    fun getText(): String
+    fun toJsonString(data: T): String
+    fun mapToJsonString(data: Map<String, Any>?): String
+    fun listOfMapsToJsonString(data: List<Map<String, Any>>?): String
+    fun mapToJsonString(data: List<Map<String, Any>>): String
 }
