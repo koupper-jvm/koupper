111ec55451e97a248f3082181ff4599a2615c2be
Jacob G. Acosta <jacob.gacosta@gmail.com>
2026-03-29 14:58:25 -0600

    feat(new): version and align local model-project template\n\n

---
+++ templates/model-project/src/main/kotlin/io/mp/handlers/RequestHandler00.kt
@@ -0,0 +1,15 @@
+package io.mp.handlers
+
+import com.amazonaws.services.lambda.runtime.Context
+import com.amazonaws.services.lambda.runtime.RequestHandler
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyRequestEvent
+import com.amazonaws.services.lambda.runtime.events.APIGatewayProxyResponseEvent
+
+{{FUNCTION_NAME}}
+
+class {{REQUEST_HANDLER_NAME}} : RequestHandler<APIGatewayProxyRequestEvent, APIGatewayProxyResponseEvent> {
+    override fun handleRequest(input: APIGatewayProxyRequestEvent?, context: Context?): APIGatewayProxyResponseEvent {
+        {{FUNCTION_NAME}}()
+        return APIGatewayProxyResponseEvent().withStatusCode(204)
+    }
+}
