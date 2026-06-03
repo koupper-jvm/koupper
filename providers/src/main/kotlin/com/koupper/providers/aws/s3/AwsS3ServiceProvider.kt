33d490c939a592ecfeec9cdcc1b0a0d7f20d24c5
Jacob G. Acosta <jacob.gacosta@gmail.com>
2026-06-02 19:40:52 -0600

    feat: implement dependency contract and namespaced shortcuts for AWS, Mailing, and GitHub providers

--- providers/src/main/kotlin/com/koupper/providers/aws/s3/AwsS3ServiceProvider.kt
+++ providers/src/main/kotlin/com/koupper/providers/aws/s3/AwsS3ServiceProvider.kt
@@ -7,4 +7,16 @@ class AwsS3ServiceProvider : ServiceProvider() {
     override fun up() {
         app.bind(S3Client::class, { S3ClientImpl() })
     }
+
+    override fun externalDependencies() = listOf(
+        "software.amazon.awssdk:s3:2.25.10",
+        "software.amazon.awssdk:s3-presigner:2.25.10"
+    )
+
+    override fun topLevelFunctions(): Map<String, String> = mapOf(
+        "s3" to """
+            import com.koupper.providers.aws.s3.S3Client
+            fun s3(): S3Client = com.koupper.container.app.getInstance(S3Client::class)
+        """.trimIndent()
+    )
 }
